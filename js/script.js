458b7a5f3918a54b9e346ece158c4d540e64e1a6
mateusdev7 <mateusdev1337@gmail.com>
2022-04-23 16:46:23 -0300

    Galeria de imagens

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ function ativarPergunta(event) {
   const resposta = document.getElementById(controls);
 
   resposta.classList.toggle("ativa");
-  const ativa = resposta.classList.contains("ativa")
+  const ativa = resposta.classList.contains("ativa");
   pergunta.setAttribute("aria-expanded", ativa);
 }
 
@@ -42,3 +42,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
